8b305e77ddef79be30afbd79466224c6e63a7c3b
ZhengYuhaoBUPT <1067992289@qq.com>
2026-04-10 13:39:20 +0800

    training final

--- run_training.sh
+++ run_training.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# Set CUDA_HOME before running accelerate
-export CUDA_HOME=/usr/local/cuda-12.8
 
-# Activate conda environment
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate zyh
+LOG_FILE="train_$(date +%Y%m%d_%H%M%S).log"
 
-# Run accelerate with the correct configuration
-accelerate launch --config_file accelerate_configs/1_node_8_gpus_deepspeed_zero2.yaml --main_process_port=8888 training/train_scmmada.py config=configs/mmada_pretraining_stage1_llada_instruct.yaml
+echo "Starting training..."
+echo "Log file: $LOG_FILE"
+
+accelerate launch \
+  --config_file accelerate_configs/1_node_8_gpus_deepspeed_zero2.yaml \
+  --main_process_port=8888 \
+  training/train_scmmada.py \
+  config=configs/scmmada_stage1.yaml \
+  > $LOG_FILE 2>&1
+
+echo "Training finished."
