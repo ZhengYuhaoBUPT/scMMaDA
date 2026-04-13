61eb54dc43f79dc09575fc6de26112b606875a12
ZhengYuhaoBUPT <1067992289@qq.com>
2026-04-13 17:00:24 +0000

    Monday Final

--- training/train_scmmada.py
+++ training/train_scmmada.py
@@ -300,16 +300,12 @@ def main():
     else:
         mask_schedule = get_mask_schedule(config.training.get("mask_schedule", "cosine"))
 
-    # 手动计算真实的微步寿命： 800 * 4 = 3200 
-    actual_scheduler_steps = config.training.max_train_steps * config.training.gradient_accumulation_steps 
-    # # 如果有 warmup，也要同步放大，防止刚起步就结束了 
-    actual_warmup_steps = config.lr_scheduler.params.warmup_steps * config.training.gradient_accumulation_steps
-    
+   
     lr_scheduler = get_scheduler(
         config.lr_scheduler.scheduler,
         optimizer=optimizer,
-        num_training_steps=actual_scheduler_steps, # config.training.max_train_steps,
-        num_warmup_steps=actual_warmup_steps, # config.lr_scheduler.params.warmup_steps,
+        num_training_steps=config.training.max_train_steps,
+        num_warmup_steps=config.lr_scheduler.params.warmup_steps,
         min_lr_scale=config.lr_scheduler.params.min_lr_scale
     )
 
